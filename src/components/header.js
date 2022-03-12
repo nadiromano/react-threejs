abd7047216b88010cc8b84ad140b51c637e3ea8d
giuseppe romano <giuseppenadir.romano@gmail.com>
2022-03-12 13:40:44 +0100

    Feat: add 3d Object, html content and Orbit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-inner">
+        <div className="logo">1460/1960</div>
+      </div>
+    </header>
+  );
+}
